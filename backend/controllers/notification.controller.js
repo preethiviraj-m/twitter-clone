9573aeec53f9c9f41615f548953bed89dfdb4371
preethiviraj-m <work.preethivi@gmail.com>
2024-09-24 23:52:21 +0530

    first commit

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,31 @@
+import Notification from "../models/notification.model.js";
+
+export const getNotifications = async(req ,res)=>{
+    try {
+        const userId = req.user._id;
+
+        const notification = await Notification.find({to : userId})
+                            .populate({
+                                path:"from",
+                                select:"username profileImg"
+                            })
+        await Notification.updateMany({to:userId},{read :true})
+        
+        res.status(200).json(notification);
+    } catch (error) {
+        console.log(`Error in getNotifications controller :${error} `)
+        res.status(500).json({error: 'Internal server error'})
+    }
+}
+export const deleteNotifications = async(req ,res)=>{
+    try {
+        const userId = req.user._id;
+
+        await Notification.deleteMany({to :userId});
+
+        res.status(200).json({message :"Notification deletd Successfully"})
+    } catch (error) {
+        console.log(`Error in deleteNotifications controller :${error} `)
+        res.status(500).json({error: 'Internal server error'})
+    }
+}
---
+++ backend/routes/auth.route.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+import { signup , login , logout , getMe} from "../controllers/auth.contoller.js";
+import protectRoute from "../middleware/protectRoute.js";
+
+const router = express.Router();
+
+router.post("/signup" , signup)
+router.post("/login" , login)
+router.post("/logout" , logout)
+router.get("/me", protectRoute, getMe)
+
+
+export default router;
